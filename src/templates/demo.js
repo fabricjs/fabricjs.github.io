864ad9856fa8ca45d7a2da040c16d2a1905a2d2a
Andrea Bogazzi <andreabogazzi79@gmail.com>
2020-06-20 17:11:46 +0200

    Adding integration for codepen (#19)
    * added integration for code-pen and made all thumbnails clickable on demos page

--- src/templates/demo.js
+++ src/templates/demo.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "../components/layoutFullWidth";
+import LayoutCodepen from "../components/layoutFullWidthWithCodepen";
 import PrevNextLinks from "../components/prevNextPostLinks/prevNextLinks";
 
 export default ({ data, pageContext }) => {
   return (
-    <Layout>
+    <LayoutCodepen>
 				<h1>{data.markdownRemark.frontmatter.title}</h1>
         <article dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
 				<PrevNextLinks prev={pageContext.prev} next={pageContext.next}></PrevNextLinks>
-    </Layout>
+    </LayoutCodepen>
   )
 }
 export const query = graphql`
