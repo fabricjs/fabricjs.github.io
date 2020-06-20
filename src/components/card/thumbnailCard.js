027d519c8c59d4c52d111d2fc6a61ce441777492
Andrea Bogazzi <andreabogazzi79@gmail.com>
2020-06-20 12:47:39 +0200

    make all thumbnails clickable in demos

--- src/components/card/thumbnailCard.js
+++ src/components/card/thumbnailCard.js
@@ -9,6 +9,7 @@ import "./thumbnailCard.css";
 export default function ThumbnailCard(props){
 	return(
 		<Card>
+			<Link title={props.title} to={props.linkToUrlSlug}>
 			<span className="thumbCntnr">
 				{props.gatsbyImgFluid?(
 					<GatsbyImage title={props.title} alt={props.title} fluid={props.gatsbyImgFluid}></GatsbyImage>
@@ -19,11 +20,12 @@ export default function ThumbnailCard(props){
 			</span>
 			{/*<span className="thumbCntnr"><img alt="For dummy purpose only" src="https://source.unsplash.com/random/200x200"/></span>*/}
 			{/*<h4>{props.title}</h4>*/}
-			<h4><Link title={props.title} to={props.linkToUrlSlug}>{props.title}</Link></h4>
+			<h4>{props.title}</h4>
 			{props.description && (<span className="desc">{props.description}</span>)}
 			{/*<span className="thumbnailCardFooter">
 				<Link title={props.title} to={props.linkToUrlSlug}>{props.buttonText || "View"}</Link>
 			</span>*/}
+			</Link>
 		</Card>
 	)
 }
