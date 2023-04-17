63a0e7aefc7f2404306484f780edd48d8f41eeff
Steve Eberhardt <melchiar2@gmail.com>
2023-04-16 21:49:49 -0700

    Update Features.jsx

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -14,14 +14,38 @@ const leftFeatures = [
   {
     title: 'Image filtering',
     description: 'WebGL and Canvas2d customizable and composable picture filters',
+  },
+  {
+    title: 'Animation',
+    description: 'Support for tweening/easing of position, transformation, and style',
+  },
+  {
+    title: 'Clip paths',
+    description: 'Create a clipping region for an object, group, or the whole canvas from any other Fabric object',
   }
 ];
 
 const rightFeatures = [
   {
+    title: 'Flexible controls',
+    description: 'On-canvas controls for scale, rotation, and skew, and a powerful custom control API',
+  },
+  {
     title: 'Typescript support',
-    description: 'WebGL and canvas2d image filters',
-  }
+    description: 'Written in Typescript for a streamlined workflow and easy debugging',
+  },
+  {
+    title: 'Object grouping',
+    description: 'Group multiple objects together for selection and transformation',
+  },
+  {
+    title: 'Viewport transformations',
+    description: 'Easily zoom & pan the canvas while maintaining render quality',
+  },
+  {
+    title: 'Object caching',
+    description: 'A powerful caching system for fast drawing of complex paths & images',
+  },
 ];
 
 export default function Features() {
