60dc84e83d9cbb3d143661c4b5af39514997facc
Andrea Bogazzi <andreabogazzi79@gmail.com>
2023-08-20 23:28:28 +0200

    remove extra console.log

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,6 @@ export default function HomePage({ data }) {
             (
               demoPage // map((demoPage)=>(
             ) => {
-              console.log(demoPage);
               const { frontmatter, fields } = demoPage;
               return (
                 <ThumbnailCard
