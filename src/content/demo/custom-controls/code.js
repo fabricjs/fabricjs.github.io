c382e776782ab9a4444952e715bff1ec65c2fee2
Michael Rienstra <mrienstra@gmail.com>
2024-06-30 23:32:16 -0700

    docs(): `custom-controls` demo, 2nd example (#117)

--- src/content/demo/custom-controls/code.js
+++ src/content/demo/custom-controls/code.js
@@ -4,8 +4,8 @@ const canvas = new fabric.Canvas(canvasEl);
 const deleteIcon =
   "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
 
-var img = document.createElement('img');
-img.src = deleteIcon;
+var deleteImg = document.createElement('img');
+deleteImg.src = deleteIcon;
 
 fabric.Object.prototype.transparentCorners = false;
 fabric.Object.prototype.cornerColor = 'blue';
@@ -41,17 +41,17 @@ document.getElementById('add').onclick = () => Add();
 
 Add();
 
-function deleteObject(eventData, transform) {
+function deleteObject(_eventData, transform) {
   const canvas = transform.target.canvas;
   canvas.remove(transform.target);
   canvas.requestRenderAll();
 }
 
-function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+function renderIcon(ctx, left, top, _styleOverride, fabricObject) {
   const size = this.cornerSize;
   ctx.save();
   ctx.translate(left, top);
   ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
-  ctx.drawImage(img, -size / 2, -size / 2, size, size);
+  ctx.drawImage(deleteImg, -size / 2, -size / 2, size, size);
   ctx.restore();
 }
