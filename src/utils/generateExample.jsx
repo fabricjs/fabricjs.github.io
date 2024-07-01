1584e1e2a3f094be35daa6e0d6dd747a5dc09f6f
Andrea Bogazzi <andreabogazzi79@gmail.com>
2024-07-01 15:49:46 +0200

    docs(): misc unfinished changes (#122)

--- src/utils/generateExample.jsx
+++ src/utils/generateExample.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
 
 export const generateExample = (codeFunc, id, codeAsString) => {
-    return ({ hideCode, canvasStyle }) => {
+    return ({ hideCode, canvasStyle, hideCanvas }) => {
         const [codestring, setCode] = useState()
         useEffect(() => {
-            console.log(codeAsString)
-            codeFunc(id);
+            hideCanvas || codeFunc(id);
             const code = codeAsString ?? codeFunc.toString();
             const matching = code.match(/function[^{]+\{\n([\s\S]*)\}$/);
             setCode(matching?.[1] || code);
@@ -16,9 +15,11 @@ export const generateExample = (codeFunc, id, codeAsString) => {
                     <code>{codestring}</code>
                 </pre>)
             }
-            <div style={canvasStyle} >
-                <canvas width="400" height="300" id={id} />
-            </div>
+            {hideCanvas ||
+                (<div style={canvasStyle} >
+                    <canvas width="400" height="300" id={id} />
+                </div>)
+            }
         </>
     }
 }
