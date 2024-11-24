2225d8caa54d0d828f27147f11ed78a8f134bdb9
Andrea Bogazzi <andreabogazzi79@gmail.com>
2024-11-24 09:41:38 +0100

    video demo

--- src/content/demo/video-element/code.js
+++ src/content/demo/video-element/code.js
@@ -1,7 +1,34 @@
-var video1El = document.getElementById('video1');
-var webcamEl = document.getElementById('webcam');
+const canvas = new fabric.Canvas(canvasEl);
+const video1El = document.createElement('video');
+const video2El = document.createElement('video');
 
-var video1 = new fabric.Image(video1El, {
+const video1source = document.createElement('source');
+const video2source = document.createElement('source');
+
+const webcamEl = document.createElement('video');
+
+video1El.width = 480;
+video1El.height = 360;
+video1El.id = 'video1'
+video1El.muted = true;
+video1El.appendChild(video1source);
+video1source.src = '/site_assets/dizzy.mp4';
+video1El.onended = () => video1El.play();
+
+video2El.width = 1280;
+video2El.height = 720;
+video2El.id = 'video2'
+video2El.muted = true;
+video2El.appendChild(video2source);
+video2source.src = '/site_assets/big-buck-bunny.mp4';
+video2El.onended = () => video2El.play();
+
+webcamEl.width = 500;
+webcamEl.height = 360;
+webcamEl.id = 'webcam'
+webcamEl.muted = true;
+
+const video1 = new fabric.FabricImage(video1El, {
   left: 200,
   top: 300,
   angle: -15,
@@ -10,7 +37,18 @@ var video1 = new fabric.Image(video1El, {
   objectCaching: false,
 });
 
-var webcam = new fabric.Image(webcamEl, {
+const video2 = new fabric.FabricImage(video2El, {
+  left: 700,
+  top: 200,
+  angle: 15,
+  originX: 'center',
+  originY: 'center',
+  objectCaching: false,
+  scaleX: 0.5,
+  scaleY: 0.5
+});
+
+var webcam = new fabric.FabricImage(webcamEl, {
   left: 539,
   top: 328,
   angle: 94.5,
@@ -19,8 +57,10 @@ var webcam = new fabric.Image(webcamEl, {
   objectCaching: false,
 });
 
-canvas.add(video1);
-video1.getElement().play();
+canvas.add(video1, video2);
+canvas.add(webcam);
+video1El.play();
+video2El.play();
 
 // Older browsers might not implement mediaDevices at all, so we set an empty object first
 if (navigator.mediaDevices === undefined) {
@@ -48,16 +88,20 @@ if (navigator.mediaDevices.getUserMedia === undefined) {
 
 
 // adding webcam video element
-navigator.mediaDevices.getUserMedia({video: true})
-  .then(function getWebcamAllowed(localMediaStream) {
-    webcamEl.srcObject = localMediaStream;
+if (window.location.protocol === 'https:') {
+    navigator.mediaDevices.getUserMedia({video: true})
+    .then(function getWebcamAllowed(localMediaStream) {
+        webcamEl.srcObject = localMediaStream;
 
-    canvas.add(webcam);
-    webcam.moveTo(0); // move webcam element to back of zIndex stack
-    webcam.getElement().play();
-  }).catch(function getWebcamNotAllowed(e) {
-  // block will be hit if user selects "no" for browser "allow webcam access" prompt
-  });
+        canvas.add(webcam);
+        webcam.moveTo(0); // move webcam element to back of zIndex stack
+        webcam.getElement().play();
+    }).catch(function getWebcamNotAllowed(e) {
+        // block will be hit if user selects "no" for browser "allow webcam access" prompt
+        console.warn('webcam was not allowed')
+    });
+}
+
 
 fabric.util.requestAnimFrame(function render() {
   canvas.renderAll();
