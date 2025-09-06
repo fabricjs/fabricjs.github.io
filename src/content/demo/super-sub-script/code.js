3517c2beef87fe2e37fdababcd6025d7eddf4f8f
aswind7 <854413241@qq.com>
2025-09-06 14:58:36 +0800

    fix super-sub-script demo (#180)

--- src/content/demo/super-sub-script/code.js
+++ src/content/demo/super-sub-script/code.js
@@ -1,26 +1,35 @@
 const $ = (id) => document.getElementById(id);
 
+const getRange = () => {
+	var active = canvas.getActiveObject();
+	if (!active) return [];
+	if (active.selectionStart === active.selectionEnd) {
+		return [active.selectionStart, active.selectionEnd + 1];
+	}
+	return [active.selectionStart, active.selectionEnd];
+}
+
 $('super').onclick = () => {
 	var active = canvas.getActiveObject();
 	if (!active) return;
-	active.setSuperscript();
+	active.setSuperscript(...getRange());
 	canvas.requestRenderAll();
 }
 
 $('sub').onclick = () => {
 	var active = canvas.getActiveObject();
 	if (!active) return;
-	active.setSubscript();
+	active.setSubscript(...getRange());
 	canvas.requestRenderAll();
 }
 
 $('remove').onclick = () => {
 	var active = canvas.getActiveObject();
 	if (!active) return;
-	active.setSelectionStyles({
+	active.setSelectionStyles && active.setSelectionStyles({
 		fontSize: undefined,
 		deltaY: undefined,
-	});
+	}, ...getRanges());
 	canvas.requestRenderAll();
 }
 
